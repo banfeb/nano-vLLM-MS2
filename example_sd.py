01a5169d61c8292f2a9d39e70339398517b20041
banfeb <115693383+banfeb@users.noreply.github.com>
2026-04-19 14:46:24 +0800

    Add example script and rejection sampler stub

---
+++ example_sd.py
@@ -0,0 +1,42 @@
+import os
+from nanovllm import LLM, SamplingParams
+from transformers import AutoTokenizer
+
+
+def main():
+    path = os.path.expanduser("/data/wxtang/model/models--Qwen--Qwen3-0.6B/snapshots/c1899de289a04d12100db370d81485cdf75e47ca")
+    tokenizer = AutoTokenizer.from_pretrained(path)
+    llm = LLM(
+        path, 
+        enforce_eager=True, 
+        tensor_parallel_size=1,
+        speculative_config={
+            "method": "ngram1",
+            "num_speculative_tokens": 2,
+            "prompt_lookup_max": 2,
+        }
+    )
+
+    sampling_params = SamplingParams(temperature=0.6, max_tokens=256)
+    prompts = [
+        "introduce yourself",
+        "list all prime numbers within 100",
+    ]
+    prompts = [
+        tokenizer.apply_chat_template(
+            [{"role": "user", "content": prompt}],
+            tokenize=False,
+            add_generation_prompt=True,
+        )
+        for prompt in prompts
+    ]
+    outputs = llm.generate(prompts, sampling_params)
+
+    for prompt, output in zip(prompts, outputs):
+        print("\n")
+        print(f"Prompt: {prompt!r}")
+        print(f"Completion: {output['text']!r}")
+
+
+if __name__ == "__main__":
+    main()
--- nanovllm/v1/sample/rejection_sampler.py
+++ nanovllm/v1/sample/rejection_sampler.py
@@ -1,3 +1,10 @@
 import torch.nn as nn
 class RejectionSampler(nn.Module):
-    pass
+    def __init__(self, sampler):
+        super().__init__()
+    
+    def forward(
+        self,
+    ):
+        pass
+        
