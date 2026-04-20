9fdf4f5387032844b49b0e689751aab49b882c31
banfeb <115693383+banfeb@users.noreply.github.com>
2026-04-20 21:38:36 +0800

    Refactor fused MoE expert layer setup

--- nanovllm/layers/fused_moe/layer.py
+++ nanovllm/layers/fused_moe/layer.py
@@ -1,41 +1,25 @@
 import torch
 import torch.nn as nn
+from nanovllm.layers.linear import MergedColumnParallelLinear, RowParallelLinear
 from nanovllm.layers.fused_moe.router.fused_topk_router import (FusedTopKRouter)
 from nanovllm.layers.fused_moe.unquantized_fused_moe_method import (
     UnquantizedFusedMoEMethod,
 )
 
-# _ExpertLinear, _MergedExpertLinear, _ExpertMLP这3个类只是为了兼容模型加载逻辑
-class _ExpertLinear(nn.Module):
-    def __init__(self, out_features: int, in_features: int):
-        super().__init__()
-        self.weight = nn.Parameter(torch.empty(out_features, in_features))
-        self.weight.weight_loader = self.weight_loader
-
-    def weight_loader(self, param: nn.Parameter, loaded_weight: torch.Tensor):
-        param.data.copy_(loaded_weight)
-
-
-class _MergedExpertLinear(_ExpertLinear):
-    def __init__(self, hidden_size: int, intermediate_size: int):
-        self.intermediate_size = intermediate_size
-        super().__init__(2 * intermediate_size, hidden_size)
-
-    def weight_loader(
-        self,
-        param: nn.Parameter,
-        loaded_weight: torch.Tensor,
-        loaded_shard_id: int,
-    ):
-        shard_offset = loaded_shard_id * self.intermediate_size
-        param.data.narrow(0, shard_offset, self.intermediate_size).copy_(loaded_weight)
-
-
 class _ExpertMLP(nn.Module):
+    # 复用 linear.py 中已有的并行线性层和 weight_loader 逻辑。
     def __init__(self, hidden_size: int, intermediate_size: int):
         super().__init__()
-        self.gate_up_proj = _MergedExpertLinear(hidden_size, intermediate_size)
-        self.down_proj = _ExpertLinear(hidden_size, intermediate_size)
+        self.gate_up_proj = MergedColumnParallelLinear(
+            hidden_size,
+            [intermediate_size] * 2,
+            bias=False,
+        )
+        self.down_proj = RowParallelLinear(
+            intermediate_size,
+            hidden_size,
+            bias=False,
+        )
 
 
 class FusedMoE(nn.Module):
@@ -109,11 +93,11 @@ class FusedMoE(nn.Module):
             router_logits=router_logits,
         )
         
-        findal_hidden_states = self.quant_method.apply(
+        final_hidden_states = self.quant_method.apply(
             layer=self,
             x=hidden_states,
             topk_weights=topk_weights,
             topk_ids=topk_ids,
         )
-        return findal_hidden_states
+        return final_hidden_states
         
